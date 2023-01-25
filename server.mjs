2c0e511d473ce936b34c0c23839549b37a7486c1
Pannathon <pannathon@idpassglobal.com>
2023-01-25 17:30:30 +0700

    server socket.io

--- server.mjs
+++ server.mjs
@@ -1,5 +1,43 @@
 import { createServer } from 'http';
 
+
+
+
+
+const app = require('express')();
+const http = require('http').Server(app);
+http.listen(3000)
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+    }
+});
+
+
+io.on('connection', client => {
+    console.log('user connected')
+
+
+    // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
+    client.on('message', function (message) {
+        console.log("message: ", message);
+        io.sockets.emit("messageBox", message)
+    })
+    
+    // setInterval(()=> {
+    //     io.sockets.emit("count",i++)
+    // },1000)
+
+    // เมื่อ Client ตัดการเชื่อมต่อ
+    client.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+})
+
+
+
+
 createServer((req, res) => {
   res.write('Hello World!');
   res.end();
