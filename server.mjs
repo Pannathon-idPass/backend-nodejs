3a9bda9cb3379e970e241c441dbd134daf7d7d67
Pannathon <pannathon@idpassglobal.com>
2023-01-26 09:04:54 +0700

    comment socket

--- server.mjs
+++ server.mjs
@@ -7,31 +7,32 @@ import { io } from 'socket.io';
 // const http = require('http').Server(app);
 // http.listen(3000)
 // const io = require("socket.io");
-io.on('connection', client => {
-    console.log('user connected')
+
+// io.on('connection', client => {
+//     console.log('user connected')
 
 
-    // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
-    client.on('message', function (message) {
-        console.log("message: ", message);
-        io.sockets.emit("messageBox", message)
-    })
+//     // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
+//     client.on('message', function (message) {
+//         console.log("message: ", message);
+//         io.sockets.emit("messageBox", message)
+//     })
     
-    // setInterval(()=> {
-    //     io.sockets.emit("count",i++)
-    // },1000)
+//     // setInterval(()=> {
+//     //     io.sockets.emit("count",i++)
+//     // },1000)
 
-    // เมื่อ Client ตัดการเชื่อมต่อ
-    client.on('disconnect', () => {
-        console.log('user disconnected')
-    })
+//     // เมื่อ Client ตัดการเชื่อมต่อ
+//     client.on('disconnect', () => {
+//         console.log('user disconnected')
+//     })
 
-})
+// })
 
 
 
 createServer((req, res) => {
 //   res.write('Hello World!');
 //   res.end();
-}).listen(process.env.PORT);
+}).listen(process.env.PORT || 3000);
 
