debd8941ef6f36a8f6e9241bbd1b5fe5333f2daa
Pannathon <pannathon@idpassglobal.com>
2023-01-26 14:19:04 +0700

    socket.io js

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-
-const http = require('http');
-
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 // const hostname = '127.0.0.1';
 // const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+
+io.on('connection', client => {
+    console.log('user connected')
+
+
+    // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
+    client.on('message', function (message) {
+        console.log("message: ", message);
+        io.sockets.emit("messageBox", message)
+    })
+    
+    // setInterval(()=> {
+    //     io.sockets.emit("count",i++)
+    // },1000)
+
+    // เมื่อ Client ตัดการเชื่อมต่อ
+    client.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+})
 
 
 
@@ -17,29 +33,5 @@ server.listen( process.env.PORT, () => {
   console.log(`Server running at ${process.env.PORT || 3000}`);
 });
 
-
-
-
-
-// io.on('connection', client => {
-//     console.log('user connected')
-
-
-//     // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
-//     client.on('message', function (message) {
-//         console.log("message: ", message);
-//         io.sockets.emit("messageBox", message)
-//     })
-    
-//     // setInterval(()=> {
-//     //     io.sockets.emit("count",i++)
-//     // },1000)
-
-//     // เมื่อ Client ตัดการเชื่อมต่อ
-//     client.on('disconnect', () => {
-//         console.log('user disconnected')
-//     })
-
-// })
 
 
