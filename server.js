a1d19072efb52fb799a9d3104b3fd3b40730edf8
Pannathon <pannathon@idpassglobal.com>
2023-01-26 17:20:02 +0700

    timet test

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ const io = require('socket.io')(server, {
 });
 // const hostname = '127.0.0.1';
 // const port = 3000;
-cron.schedule('10 17 * * *', function(){
-  console.log('running every  at 17:10:00  : ' + new Date().toString());
+cron.schedule('30 21 17 * * *', function(){
+  console.log('running every  at 17:21:30  : ' + new Date().toString());
   var autoMessage = {
     username: '--System--',
     message: 'https://youtu.be/HTHpkQJ3pVI'
   }
   io.sockets.emit("messageBox", autoMessage)
+},{
+  scheduled: true,
+  timezone: "Asia/Bangkok"
 });    
 
 
