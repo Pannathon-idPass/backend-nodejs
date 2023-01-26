5d07e9ed38941a786c00727da5358afe53981fb4
Pannathon <pannathon@idpassglobal.com>
2023-01-26 10:20:07 +0700

    server

--- server.mjs
+++ server.mjs
@@ -1,8 +1,6 @@
-// import { createServer } from 'http';
-// import { io } from 'socket.io';
 
 import { createServer } from 'http';
-import { io} from 'socket.io'
+// import { io} from 'socket.io'
 createServer((req, res) => {
   res.write('Hello World!');
   res.end();
@@ -10,26 +8,26 @@ createServer((req, res) => {
 
 // const io = require("socket.io");
 
-io.on('connection', client => {
-    console.log('user connected')
+// io.on('connection', client => {
+//     console.log('user connected')
 
 
-    // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
-    client.on('message', function (message) {
-        console.log("message: ", message);
-        io.sockets.emit("messageBox", message)
-    })
+//     // ส่งข้อมูลไปยัง Client ทุกตัวที่เขื่อมต่อแบบ Realtime
+//     client.on('message', function (message) {
+//         console.log("message: ", message);
+//         io.sockets.emit("messageBox", message)
+//     })
     
-    // setInterval(()=> {
-    //     io.sockets.emit("count",i++)
-    // },1000)
+//     // setInterval(()=> {
+//     //     io.sockets.emit("count",i++)
+//     // },1000)
 
-    // เมื่อ Client ตัดการเชื่อมต่อ
-    client.on('disconnect', () => {
-        console.log('user disconnected')
-    })
+//     // เมื่อ Client ตัดการเชื่อมต่อ
+//     client.on('disconnect', () => {
+//         console.log('user disconnected')
+//     })
 
-})
+// })
 
 
 
